9072d7ece9c125cb2fe75a2ddb6db283a44291b2
PRASUN-SITAULA <psitaula209@gmail.com>
2023-01-04 21:39:03 +0545

    Insertion

--- Linked List/insertion.cpp
+++ Linked List/insertion.cpp
@@ -5,7 +5,6 @@ struct Node{
     Node *next;
 };
 
-
 Node* start = NULL;
 Node *temp = new Node;
 
@@ -26,11 +25,43 @@ int insertAtEnd(int item){
     temp -> next = newNode;
     newNode -> data = item;
     newNode -> next = NULL;
+    return 0;
 }
 
 
-int insertAfterSpecificNode(){
+int insertAfterSpecificNode(int value, int item){
+    temp = start;
+    Node *newNode = new Node;
+    while(temp -> data != value){
+        temp = temp -> next;
+    }
+    newNode -> next = temp -> next;
+    temp -> next = newNode;
+    newNode -> data = item;
+    return 0;
+}
 
+
+int insertBeforeSpecificNode(int value, int item){
+    temp = start;
+    Node *newNode = new Node;
+    Node *pretemp = new Node;
+    pretemp = temp;
+    while(temp -> data != value){
+        pretemp = temp;
+        temp = temp -> next;
+    }
+    // if the element is to be inserted before first element
+    if(pretemp == start){
+        newNode -> data = item;
+        newNode -> next = start;
+        start = newNode;
+    }
+    else{
+        newNode -> next = pretemp -> next;
+        pretemp -> next = newNode;
+        newNode -> data = item;
+    }
     return 0;
 }
 
@@ -52,7 +83,8 @@ int main()
     insertAtBeginning(45);
     insertAtEnd(10);
     insertAtEnd(5);
+    insertAfterSpecificNode(30, 50);
+    insertBeforeSpecificNode(45, 90);
     displayNodeElements();
-
     return 0;
 }
